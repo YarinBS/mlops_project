0acc490a2bbd4fabf1ca694ea1c050e493efc7ba
YarinB <yarin10121@gmail.com>
2026-01-08 22:50:09 +0200

    W&B and Pytorch Lightning integrations

--- src/mlops_project/model.py
+++ src/mlops_project/model.py
@@ -1,3 +1,4 @@
+import pytorch_lightning as pl
 import torch
 from torch import nn
 
@@ -43,6 +44,73 @@ class MyAwesomeModel(nn.Module):
         x = self.dropout(x)
         x = self.fc1(x)
         return x
+
+
+class MyAwesoneLightningModel(pl.LightningModule):
+    def __init__(
+            self,
+            config=None,
+            in_channels: int = 1, 
+            out_channels_1: int = 32, 
+            out_channels_2: int = 64, 
+            out_channels_3: int = 128,
+            kernel_size: int = 3, 
+            stride: int = 1,
+            dropout: float = 0.5,
+            lr: float = 1e-3) -> None:
+        super().__init__()
+
+        if not config:
+            self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels_1, kernel_size=kernel_size, stride=stride)
+            self.conv2 = nn.Conv2d(in_channels=out_channels_1, out_channels=out_channels_2, kernel_size=kernel_size, stride=stride)
+            self.conv3 = nn.Conv2d(in_channels=out_channels_2, out_channels=out_channels_3, kernel_size=kernel_size, stride=stride)
+            self.dropout = nn.Dropout(p=dropout)
+            self.fc1 = nn.Linear(in_features=128, out_features=10)
+
+            self.lr = lr
+            self.criterion = torch.nn.CrossEntropyLoss()
+        else:
+            self.conv1 = nn.Conv2d(in_channels=config.in_channels, out_channels=config.out_channels_1, kernel_size=config.kernel_size, stride=config.stride)
+            self.conv2 = nn.Conv2d(in_channels=config.out_channels_1, out_channels=config.out_channels_2, kernel_size=config.kernel_size, stride=config.stride)
+            self.conv3 = nn.Conv2d(in_channels=config.out_channels_2, out_channels=config.out_channels_3, kernel_size=config.kernel_size, stride=config.stride)
+            self.dropout = nn.Dropout(p=config.dropout)
+            self.fc1 = nn.Linear(in_features=128, out_features=10)
+
+            self.lr = config.lr
+            self.criterion = torch.nn.CrossEntropyLoss()
+    
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, kernel_size=2, stride=2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, kernel_size=2, stride=2)
+        x = torch.relu(self.conv3(x))
+        x = torch.max_pool2d(x, kernel_size=2, stride=2)
+        x = torch.flatten(x, start_dim=1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+        return x
+
+    def training_step(self, batch):
+        img, target = batch
+        output = self(img)
+        loss = self.criterion(output, target)
+        acc = (output.argmax(dim=1) == target).float().mean()
+        self.log("train_loss", loss, prog_bar=True, on_epoch=True, on_step=False)
+        self.log("train_acc", acc, prog_bar=True, on_epoch=True, on_step=False)
+        return loss
+
+    def validation_step(self, batch) -> None:
+        data, target = batch
+        preds = self(data)
+        loss = self.criterion(preds, target)
+        acc = (target == preds.argmax(dim=-1)).float().mean()
+        self.log('val_loss', loss, on_epoch=True)
+        self.log('val_acc', acc, on_epoch=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
 
 
 if __name__ == "__main__":
--- src/mlops_project/train.py
+++ src/mlops_project/train.py
@@ -3,10 +3,12 @@ import logging
 import matplotlib.pyplot as plt
 from omegaconf import DictConfig, OmegaConf
 import os
+import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
+import wandb
 
-from mlops_project.model import MyAwesomeModel
+from mlops_project.model import MyAwesomeModel, MyAwesoneLightningModel
 from mlops_project.data import corrupt_mnist
 
 logger = logging.getLogger(__name__)
@@ -24,61 +26,88 @@ def train(config: DictConfig) -> None:
     """
 
     logger.info(f"Config:\n{OmegaConf.to_yaml(config)}")
-
     torch.manual_seed(config.seed)
 
-    model = MyAwesomeModel(
-        in_channels=config.in_channels,
-        out_channels_1=config.out_channels_1,
-        out_channels_2=config.out_channels_2,
-        out_channels_3=config.out_channels_3,
-        kernel_size=config.kernel_size,
-        stride=config.stride,
-        dropout=config.dropout,
+    wandb.init(
+        project="mlops_project",
+        config={"lr": config.lr, "batch_size": config.batch_size, "epochs": config.epochs},
+    )
+
+    # model = MyAwesomeModel(
+    #     in_channels=config.in_channels,
+    #     out_channels_1=config.out_channels_1,
+    #     out_channels_2=config.out_channels_2,
+    #     out_channels_3=config.out_channels_3,
+    #     kernel_size=config.kernel_size,
+    #     stride=config.stride,
+    #     dropout=config.dropout,
+    # ).to(DEVICE)
+
+    model = MyAwesoneLightningModel(
+        config=config
     ).to(DEVICE)
+
     train_set, _ = corrupt_mnist()
-    train_loader = DataLoader(train_set, batch_size=config.batch_size)
+    train_loader = DataLoader(train_set, batch_size=config.batch_size, shuffle=True)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
-    criterion = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+    # criterion = torch.nn.CrossEntropyLoss()
 
-    train_losses, train_accuracies = [], []
+    # train_losses, train_accuracies = [], []
 
-    for epoch in range(config.epochs):
-        model.train()
-        train_loss = 0.0
-        train_acc = 0.0
+    # for epoch in range(config.epochs):
+    #     model.train()
+    #     train_loss = 0.0
+    #     train_acc = 0.0
 
-        for x, y in train_loader:
-            x, y = x.to(DEVICE), y.to(DEVICE)
-            optimizer.zero_grad()
-            output = model(x)
-            loss = criterion(output, y)
-            loss.backward()
-            optimizer.step()
+    #     for x, y in train_loader:
+    #         x, y = x.to(DEVICE), y.to(DEVICE)
+    #         optimizer.zero_grad()
+    #         output = model(x)
+    #         loss = criterion(output, y)
+    #         loss.backward()
+    #         optimizer.step()
 
-            train_loss += loss.item() * x.size(0)  # accumulating loss, taking into account the batch size
-            _, preds = torch.max(output, 1)
-            train_acc += torch.sum(preds == y).item()
+    #         train_loss += loss.item() * x.size(0)  # accumulating loss, taking into account the batch size
+    #         _, preds = torch.max(output, 1)
+    #         train_acc += torch.sum(preds == y).item()
 
-        train_loss /= len(train_loader.dataset)
-        train_losses.append(train_loss)
+    #     train_loss /= len(train_loader.dataset)
+    #     train_losses.append(train_loss)
 
-        train_acc /= len(train_loader.dataset)
-        train_accuracies.append(train_acc)
+    #     train_acc /= len(train_loader.dataset)
+    #     train_accuracies.append(train_acc)
 
-        logger.info(f"Epoch {epoch + 1}/{config.epochs}, Train loss: {train_loss:.4f}, Train accuracy: {train_acc:.4f}")
+    #     logger.info(f"Epoch {epoch + 1}/{config.epochs}, Train loss: {train_loss:.4f}, Train accuracy: {train_acc:.4f}")
+    #     wandb.log({"epoch": epoch + 1, "train_loss": train_loss, "train_accuracy": train_acc})
+
+    trainer = pl.Trainer(
+        max_epochs=config.epochs,
+        logger=pl.loggers.WandbLogger(project="mlops_project"),
+        accelerator="auto",
+        devices=1
+    )
+    trainer.fit(model, train_loader)
 
     logger.info("Training complete")
-    torch.save(model.state_dict(), config.model_save_path)
 
-    # Plotting training loss
-    plt.plot(range(1, config.epochs + 1), train_losses, label="Train Loss")
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.title("Training Loss over Epochs")
-    plt.legend()
-    plt.savefig(os.path.join(config.figures_dir, "training_loss.png"))
+    # Saving model
+    torch.save(model.state_dict(), config.model_save_path)
+    artifact = wandb.Artifact("trained_model", type="model")
+    artifact.add_file(config.model_save_path)
+    logged_artifact = wandb.log_artifact(artifact)
+    wandb.run.link_artifact(artifact=logged_artifact, target_path="myregistry/models")
+
+    # # Plotting training loss
+    # plt.plot(range(1, config.epochs + 1), train_losses, label="Train Loss")
+    # plt.xlabel("Epochs")
+    # plt.ylabel("Loss")
+    # plt.title("Training Loss over Epochs")
+    # plt.legend()
+    # plt.savefig(os.path.join(config.figures_dir, "training_loss.png"))
+    # wandb.log(
+    #     {"training_loss_plot": wandb.Image(os.path.join(config.figures_dir, "training_loss.png"), caption="Training Loss over Epochs")}
+    # )
 
 
 if __name__ == "__main__":
