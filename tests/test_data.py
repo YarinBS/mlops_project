d73efb37f3434e6b0f329822738fd7fe4cc19ecc
YarinB <yarin10121@gmail.com>
2026-01-12 22:34:02 +0200

    Update data test to use pytest-mock

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,12 +1,10 @@
 import torch
 from torch.utils.data import TensorDataset
-from unittest.mock import patch
 
 from mlops_project.data import corrupt_mnist
 
 
-def test_my_dataset():
-
+def test_my_dataset(mocker):
     mock_train = TensorDataset(
         torch.randn(50000, 1, 28, 28),
         torch.randint(0, 10, (50000,))
@@ -16,17 +14,18 @@ def test_my_dataset():
         torch.randint(0, 10, (5000,))
     )
 
-    with patch('mlops_project.data.corrupt_mnist', return_value=(mock_train, mock_test)):
-        train, test = corrupt_mnist()
-        assert len(train) == 50000
-        assert len(test) == 5000
+    mocker.patch('mlops_project.data.corrupt_mnist', return_value=(mock_train, mock_test))
 
-        for dataset in [train, test]:
-            for x, y in dataset:
-                assert x.shape == (1, 28, 28)
-                assert y in range(10)
-        
-        train_targets = torch.unique(train.tensors[1])
-        assert (train_targets == torch.arange(0,10)).all()
-        test_targets = torch.unique(test.tensors[1])
-        assert (test_targets == torch.arange(0,10)).all()
+    train, test = corrupt_mnist()
+    assert len(train) == 50000
+    assert len(test) == 5000
+
+    for dataset in [train, test]:
+        for x, y in dataset:
+            assert x.shape == (1, 28, 28)
+            assert y in range(10)
+    
+    train_targets = torch.unique(train.tensors[1])
+    assert (train_targets == torch.arange(0,10)).all()
+    test_targets = torch.unique(test.tensors[1])
+    assert (test_targets == torch.arange(0,10)).all()
